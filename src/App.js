027b2cdc428342259a69c52432f4141ef4bcf908
Robert Rozehnal <rob.kab.roz@gmail.com>
2022-05-29 16:29:56 +0200

    saved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import classes from './App.module.scss';
+import Form from './components/Form';
 
 const App = () => {
   return (
     <div className={classes.container}>
       <h1>freeCodeCamp Survey Form</h1>
       <p>Thank you for taking the time to help us improve the platform</p>
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./Form.module.scss";
+
+const Form = () => {
+
+  return (
+    <div className={classes.container}>
+      <label>Name</label>
+      <input></input>
+    </div>
+  );
+};
+
+export default Form;
